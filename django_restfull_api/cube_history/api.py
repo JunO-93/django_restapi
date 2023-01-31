33677c9db500349a04bf62a7e991f01e9cfcf775
JunOLee <45375353+JunO-93@users.noreply.github.com>
2023-01-31 10:20:14 +0900

    #6 외부 API 호출 구현

--- django_restfull_api/cube_history/api.py
+++ django_restfull_api/cube_history/api.py
@@ -6,33 +6,22 @@ import json
 import requests
 import pandas as pd
 
-serviceKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJYLUFwcC1SYXRlLUxpbWl0IjoiMjAwMDA6MTAiLCJhY2NvdW50X2lkIjoiNDg2NjQ0ODIzIiwiYXV0aF9pZCI6IjQiLCJleHAiOjE3MzY4MTM0MTYsImlhdCI6MTY3Mzc0MTQxNiwibmJmIjoxNjczNzQxNDE2LCJzZXJ2aWNlX2lkIjoiNDMwMDExMzk3IiwidG9rZW5fdHlwZSI6IkFjY2Vzc1Rva2VuIn0.FN0ZQrkhRJ7TpSU04AIo5dpvQwoQZvsu2mVjK25lrtw'
-serviceKeyDecoded = unquote(serviceKey, 'UTF-8')
+def apiCall(count : int , date : str, cursor = ''):
+    
+    serviceKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJYLUFwcC1SYXRlLUxpbWl0IjoiMjAwMDA6MTAiLCJhY2NvdW50X2lkIjoiNDg2NjQ0ODIzIiwiYXV0aF9pZCI6IjQiLCJleHAiOjE3MzY4MTM0MTYsImlhdCI6MTY3Mzc0MTQxNiwibmJmIjoxNjczNzQxNDE2LCJzZXJ2aWNlX2lkIjoiNDMwMDExMzk3IiwidG9rZW5fdHlwZSI6IkFjY2Vzc1Rva2VuIn0.FN0ZQrkhRJ7TpSU04AIo5dpvQwoQZvsu2mVjK25lrtw'
+    serviceKeyDecoded = unquote(serviceKey, 'UTF-8')
 
-def apiCall(count : int , date : str, cursor = None):
-    # api query param 
-    url = 'https://public.api.nexon.com/openapi/maplestory/v1/cube-use-results'
-    returntype = 'json'
-    # count = '10'
-    # date = '2022-01-15'
-    # cursor = ''
-
-    queryParams = '?' + urlencode({quote_plus('ServiceKey') : serviceKeyDecoded, quote_plus('returnType') : returntype ,
-    quote_plus('count') : count , quote_plus('date') : date , quote_plus('cursor') : cursor})
-
-    request = Request(url + queryParams + "&_type=json")
-    request.get_method = lambda:'GET'
-    response_body = urlopen(request).read()
-
-    getJson = json.loads(response_body)["response"]["body"]
-    retItems = {}
-    i = 0
-
-    #make dict datas
-    for item in getJson["items"]["item"]:
-        item = dict(item.items())
-        retItems['item'] = json.dumps(item, ensure_ascii=False)        
-
-    print(f'2::{retItems}')
-
-    return retItems 
+    url = f'https://public.api.nexon.com/openapi/maplestory/v1/cube-use-results?count={count}&date={date}&cursor={cursor}'  
+    headers = {
+        'Content-Type': 'application/json',
+        'charset': 'UTF-8',
+        'Accept': '*/*',
+        'authorization' : serviceKey,
+    }
+    
+    try:
+        response = requests.get(url, headers = headers)        
+        contents = response.text
+    except Exception as e:
+        print(e)    
+    return contents
--- django_restfull_api/cube_history/views.py
+++ django_restfull_api/cube_history/views.py
@@ -13,41 +13,29 @@ from django.views import generic
 import json
 import pandas as pd
 
-serviceKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJYLUFwcC1SYXRlLUxpbWl0IjoiMjAwMDA6MTAiLCJhY2NvdW50X2lkIjoiNDg2NjQ0ODIzIiwiYXV0aF9pZCI6IjQiLCJleHAiOjE3MzY4MTM0MTYsImlhdCI6MTY3Mzc0MTQxNiwibmJmIjoxNjczNzQxNDE2LCJzZXJ2aWNlX2lkIjoiNDMwMDExMzk3IiwidG9rZW5fdHlwZSI6IkFjY2Vzc1Rva2VuIn0.FN0ZQrkhRJ7TpSU04AIo5dpvQwoQZvsu2mVjK25lrtw'
-serviceKeyDecoded = unquote(serviceKey, 'UTF-8')
-
 # Create your views here.
 # class DataViewSet(viewsets.ModelViewSet):
 #     queryset = data.objects.all()
 #     serializer_class = DataSerializer
 
+# 로깅세팅
 logger = logging.getLogger()
 logger.setLevel(logging.DEBUG)
-
 
 def valueInputSet(request):
     return render(request, 'html/valueInput.html') 
 
-
 def selectCubeData(request):
     
-    if request.method =='POST':
+    if request.method =='POST': 
    
         try:
             count = request.POST['count']
-            date = request.POST['date']            
-            print(f'count : {count}, date : {date}')
+            date = request.POST['date']                        
+
+            cubeHis = apiCall(count, date)
+    
         except Exception as e:            
             logger.debug(e)      
         
-    return HttpResponse(f'count : {count}, date : {date}')
-
-#     template_name = 'html/contact.html'
-
-#     def post(self , request):
-#         inputkw = request.POST.get('name')
-#         result = apiCall.kewordFindAPI(inputkw)
-#         context = {
-#             'result' : result             
-#         }
-#         return render (request, self.template_name, context)
+    return HttpResponse(cubeHis)
